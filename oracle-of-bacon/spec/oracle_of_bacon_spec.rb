ba7630d021cd165e7e0e99d324d3f88d3e6e0d5f
englerjose <englerjosegithub@gmail.com>
2015-07-30 11:56:14 +1200

    Update to rspec3

--- oracle-of-bacon/spec/oracle_of_bacon_spec.rb
+++ oracle-of-bacon/spec/oracle_of_bacon_spec.rb
@@ -9,62 +9,137 @@ describe OracleOfBacon do
     before(:each) { @orb = OracleOfBacon.new('fake_api_key') }
     describe 'when new' do
       subject { @orb }
-      it { should_not be_valid }
+      it { is_expected.not_to be_valid }
     end
     describe 'when only From is specified' do
       subject { @orb.from = 'Carrie Fisher' ; @orb }
-      it { should be_valid }
-      its(:from) { should == 'Carrie Fisher' }
-      its(:to)   { should == 'Kevin Bacon' }
+      it { is_expected.to be_valid }
+
+      describe '#from' do
+        subject { super().from }
+        it { is_expected.to eq('Carrie Fisher') }
+      end
+
+      describe '#to' do
+        subject { super().to }
+        it { is_expected.to eq('Kevin Bacon') }
+      end
     end
     describe 'when only To is specified' do
       subject { @orb.to = 'Ian McKellen' ; @orb }
-      it { should be_valid }
-      its(:from) { should == 'Kevin Bacon' }
-      its(:to)   { should == 'Ian McKellen'}
+      it { is_expected.to be_valid }
+
+      describe '#from' do
+        subject { super().from }
+        it { is_expected.to eq('Kevin Bacon') }
+      end
+
+      describe '#to' do
+        subject { super().to }
+        it { is_expected.to eq('Ian McKellen')}
+      end
     end
     describe 'when From and To are both specified' do
       context 'and distinct' do
         subject { @orb.to = 'Ian McKellen' ; @orb.from = 'Carrie Fisher' ; @orb }
-        it { should be_valid }
-        its(:from) { should == 'Carrie Fisher' }
-        its(:to)   { should == 'Ian McKellen'  }
+        it { is_expected.to be_valid }
+
+        describe '#from' do
+          subject { super().from }
+          it { is_expected.to eq('Carrie Fisher') }
+        end
+
+        describe '#to' do
+          subject { super().to }
+          it { is_expected.to eq('Ian McKellen')  }
+        end
       end
       context 'and the same' do
         subject {  @orb.to = @orb.from = 'Ian McKellen' ; @orb }
-        it { should_not be_valid }
+        it { is_expected.not_to be_valid }
       end
     end
   end
   describe 'parsing XML response', :pending => true do
     describe 'for unauthorized access/invalid API key' do
       subject { OracleOfBacon::Response.new(File.read 'spec/unauthorized_access.xml') }
-      its(:type) { should == :error }
-      its(:data) { should == 'Unauthorized access' }
+
+      describe '#type' do
+        subject { super().type }
+        it { is_expected.to eq(:error) }
+      end
+
+      describe '#data' do
+        subject { super().data }
+        it { is_expected.to eq('Unauthorized access') }
+      end
     end
     describe 'for a normal match' do
       subject { OracleOfBacon::Response.new(File.read 'spec/graph_example.xml') }
-      its(:type) { should == :graph }
-      its(:data) { should == ['Carrie Fisher', 'Under the Rainbow (1981)',
-                              'Chevy Chase', 'Doogal (2006)', 'Ian McKellen'] }
+
+      describe '#type' do
+        subject { super().type }
+        it { is_expected.to eq(:graph) }
+      end
+
+      describe '#data' do
+        subject { super().data }
+        it { is_expected.to eq(['Carrie Fisher', 'Under the Rainbow (1981)',
+                              'Chevy Chase', 'Doogal (2006)', 'Ian McKellen']) }
+      end
     end
     describe 'for a normal match (backup)' do
       subject { OracleOfBacon::Response.new(File.read 'spec/graph_example2.xml') }
-      its(:type) { should == :graph }
-      its(:data) { should == ["Ian McKellen", "Doogal (2006)", "Kevin Smith (I)",
-                              "Fanboys (2009)", "Carrie Fisher"] }
+
+      describe '#type' do
+        subject { super().type }
+        it { is_expected.to eq(:graph) }
+      end
+
+      describe '#data' do
+        subject { super().data }
+        it { is_expected.to eq(["Ian McKellen", "Doogal (2006)", "Kevin Smith (I)",
+                              "Fanboys (2009)", "Carrie Fisher"]) }
+      end
     end
     describe 'for a spellcheck match' do
       subject { OracleOfBacon::Response.new(File.read 'spec/spellcheck_example.xml') }
-      its(:type) { should == :spellcheck }
-      its(:data) { should have(34).elements }
-      its(:data) { should include('Anthony Perkins (I)') }
-      its(:data) { should include('Anthony Parkin') }
+
+      describe '#type' do
+        subject { super().type }
+        it { is_expected.to eq(:spellcheck) }
+      end
+
+      describe '#data' do
+        subject { super().data }
+
+        it 'has 34 elements' do
+          expect(subject.size).to eq(34)
+        end
+      end
+
+      describe '#data' do
+        subject { super().data }
+        it { is_expected.to include('Anthony Perkins (I)') }
+      end
+
+      describe '#data' do
+        subject { super().data }
+        it { is_expected.to include('Anthony Parkin') }
+      end
     end
     describe 'for unknown response' do
       subject { OracleOfBacon::Response.new(File.read 'spec/unknown.xml') }
-      its(:type) { should == :unknown }
-      its(:data) { should match /unknown/i }
+
+      describe '#type' do
+        subject { super().type }
+        it { is_expected.to eq(:unknown) }
+      end
+
+      describe '#data' do
+        subject { super().data }
+        it { is_expected.to match /unknown/i }
+      end
     end
   end
   describe 'constructing URI', :pending => true do
@@ -74,27 +149,27 @@ describe OracleOfBacon do
       oob.make_uri_from_arguments
       oob.uri
     end
-    it { should match(URI::regexp) }
-    it { should match /p=fake_key/ }
-    it { should match /b=George\+Clooney/ }
-    it { should match /a=3%252\+%22a/ }
+    it { is_expected.to match(URI::regexp) }
+    it { is_expected.to match /p=fake_key/ }
+    it { is_expected.to match /b=George\+Clooney/ }
+    it { is_expected.to match /a=3%252\+%22a/ }
   end
   describe 'service connection', :pending => true do
     before(:each) do
       @oob = OracleOfBacon.new
-      @oob.stub(:valid?).and_return(true)
+      allow(@oob).to receive(:valid?).and_return(true)
     end
     it 'should create XML if valid response' do
       body = File.read 'spec/graph_example.xml'
       FakeWeb.register_uri(:get, %r(http://oracleofbacon\.org), :body => body)
-      OracleOfBacon::Response.should_receive(:new).with(body)
+      expect(OracleOfBacon::Response).to receive(:new).with(body)
       @oob.find_connections
     end
     it 'should raise OracleOfBacon::NetworkError if network problem' do
       FakeWeb.register_uri(:get, %r(http://oracleofbacon\.org),
         :exception => Timeout::Error)
-      lambda { @oob.find_connections }.
-        should raise_error(OracleOfBacon::NetworkError)
+      expect { @oob.find_connections }.
+        to raise_error(OracleOfBacon::NetworkError)
     end
   end
 
